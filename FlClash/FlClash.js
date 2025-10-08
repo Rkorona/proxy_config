62f5d8fb23de687143e5a618d51a6da1dedb6af2
Rkorona <korona.starx@gmail.com>
2025-10-08 01:44:12 +0000

    update js files

--- FlClash/FlClash.js
+++ FlClash/FlClash.js
@@ -1,12 +1,15 @@
-  const yaml = ProxyUtils.yaml.safeLoad($content ?? $files[0])
-  let clashMetaProxies = await produceArtifact({
-    type: 'collection',
-    name: 'FlClash',
-    platform: 'ClashMeta',
-    produceType: 'internal'
-  })
-  
-  
-  yaml.proxies.unshift(...clashMetaProxies)
-  $content = ProxyUtils.yaml.dump(yaml)
-  
+const yaml = ProxyUtils.yaml.safeLoad($content ?? $files[0])
+/**
+let clashMetaProxies = await produceArtifact({
+  type: 'collection',
+  name: 'FlClash',
+  platform: 'ClashMeta',
+  produceType: 'internal'
+})
+**/
+
+// 使用方括号 [] 来访问带中划线的属性名
+yaml['proxy-providers'].netcup.url = ""
+
+//yaml.proxies.unshift(...clashMetaProxies)
+$content = ProxyUtils.yaml.dump(yaml)
